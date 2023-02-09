2cf1cce348e05e98d8256b75a3f78e0ab492b3de
Los-3 <66935939+Los-3@users.noreply.github.com>
2023-02-08 21:02:31 -0500

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -18,14 +18,9 @@ drone.on('open', error => {
 
   const room = drone.subscribe('observable-room', {historyCount: 100});
   room.on('history_message', ({data}) => {
-    console.log(data);
     addMessageToListDOM(data);
   });
 
-  room.on('data', data => {
-    console.log(data);
-    addMessageToListDOM(data);
-  });
   room.on('open', error => {
     if (error) {
       return console.error(error);
