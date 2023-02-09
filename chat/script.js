1b0a4ce13c2cf5172b9e33d06e1244264c429464
Los-3 <66935939+Los-3@users.noreply.github.com>
2023-02-08 20:09:09 -0500

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -17,7 +17,7 @@ drone.on('open', error => {
   console.log('Successfully connected to Scaledrone');
 
   const room = drone.subscribe('observable-room', {historyCount: 100});
-  room.on('history_message', message => createMessageElement(message));
+  room.on('history_message', message => console.log(message));
   room.on('open', error => {
     if (error) {
       return console.error(error);
